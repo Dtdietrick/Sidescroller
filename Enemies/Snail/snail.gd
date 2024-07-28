d93123535c6fa4f3b3a3412456d25a46755ec930
Dylan Dietrick <dtdietrick@gmail.com>
2024-07-28 14:54:36 -0400

    snail boundaries working

--- Enemies/Snail/snail.gd
+++ Enemies/Snail/snail.gd
@@ -5,9 +5,12 @@ extends CharacterBody2D
 @export var speed : float = 30.0
 @export var hit_state : State
 @onready var state_machine : CharacterStateMachine = $CharacterStateMachine
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var timer : Timer = $WallTimer
+var recently_turned : bool = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var direction : Vector2 = Vector2.ZERO
 func _ready():
 	animation_tree.active = true
 
@@ -15,13 +18,29 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction : Vector2 = starting_move_direction
+	
+	if(direction == Vector2.ZERO):	
+		direction = starting_move_direction
+	
+	if is_on_wall() && recently_turned == false:
+		timer.start()
+		recently_turned = true
+		direction = -direction
+		print(direction,recently_turned)
+		
 	if direction && state_machine.check_if_can_move():
 		velocity.x = direction.x * speed
 	elif state_machine.current_state != hit_state:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_facing_direction()
+
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = true
+	elif direction.x < 0:
+		sprite.flip_h = false
+	
+func _on_wall_timer_timeout():
+	recently_turned = false
